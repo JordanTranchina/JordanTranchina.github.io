298a9243f8365459acc17f8b92db9fee7639ae7d
Jordan Tranchina <Jordan12522@gmail.com>
2019-02-28 20:03:51 -0500

    Prepping for massive CSS breakpoint implementation

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -1,6 +1,10 @@
-document.getElementById('hamburger').addEventListener("click", myFunction(x));
+document.getElementById('hamburger').addEventListener("click", mobileMenu(x));
 
-function myFunction(x) {
+function mobileMenu(x) {
   x.classList.toggle("active");
   document.getElementById("overlay").classList.toggle("open");
+}
+
+function mobileDisplay() {
+    document.getElementById("overlay").classList.toggle("open");
 }
