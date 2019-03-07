c2597bde8bc9ab61bb9ddbecefe5244cc7cd2c09
Jordan Tranchina <Jordan12522@gmail.com>
2019-03-07 17:05:54 -0500

    fixing scroll issues when displaying menu overlay

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -9,6 +9,7 @@ document.getElementById('nav_tab_about').addEventListener("click", mobileMenu(x)
 function mobileMenu(x) {
   x.classList.toggle("active");
   document.getElementById("overlay").classList.toggle("open");
+  document.body.classList.add("overlay-body");
   // improve by grabing by class not by id
   document.getElementById("nav_tab_about").classList.toggle("open");
   document.getElementById("nav_tab_work").classList.toggle("open");
