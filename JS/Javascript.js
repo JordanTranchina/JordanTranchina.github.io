ce534f76f732a9823e81ef239162b104aeabb3c6
Jordan Tranchina <Jordan12522@gmail.com>
2019-03-11 10:01:43 -0400

    seperating open state styles from non-open state in nav_container before css grid commit for mobile

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -9,6 +9,7 @@ document.getElementById('nav_tab_about').addEventListener("click", mobileMenu(x)
 function mobileMenu(x) {
   x.classList.toggle("active");
   document.getElementById("overlay").classList.toggle("open");
+  // improve by switching to toggle instead of add (be concious of nav-text janking position) by class not by id
   document.body.classList.add("overlay-body");
   // improve by grabing by class not by id
   document.getElementById("nav_tab_about").classList.toggle("open");
