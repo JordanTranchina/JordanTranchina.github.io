0ae0083f238860abbfadd627d0b1c2f888ebf269
Jordan Tranchina <Jordan12522@gmail.com>
2019-03-18 19:27:47 -0400

    minor edits to javascript & adding theme color to the head space in HTML

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -1,10 +1,9 @@
-document.getElementById('hamburger').addEventListener("click", mobileMenu(x));
+document.getElementById("hamburger").addEventListener("click", mobileMenu(x));
 // return to grab by class not by id
-document.getElementById('nav_tab_about').addEventListener("click", mobileMenu(x));
-document.getElementById('nav_tab_work').addEventListener("click", mobileMenu(x));
-document.getElementById('nav_tab_skills').addEventListener("click", mobileMenu(x));
-document.getElementById('nav_tab_about').addEventListener("click", mobileMenu(x));
-
+document.getElementById("nav_tab_about").addEventListener("click", mobileMenu(x));
+document.getElementById("nav_tab_work").addEventListener("click", mobileMenu(x));
+document.getElementById("nav_tab_skills").addEventListener("click", mobileMenu(x));
+document.getElementById("nav_tab_about").addEventListener("click", mobileMenu(x));
 
 function mobileMenu(x) {
   x.classList.toggle("active");
